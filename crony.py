6277ffb607168d08d57fc249ee25a03a4cbc96df
jonathan <jonathan.fernandes@netcore.co.in>
2016-04-17 13:45:52 +0530

    'cp' command and changes
    Added help texts for commands, started working on the 'cp' command.

--- crony.py
+++ crony.py
@@ -13,6 +13,7 @@ def crony():
                       help='Number of crons to display, displays 10 by default')
 @click.option('--host', default=None, help='List crons for <host>')
 def ls(limit, host):
+    """List crontabs on a remote or local system"""
     ct = Crontab(host=host)
     cps = ct.list()
     jobs = parser.parse_file(cps.stdout, limit)
@@ -26,11 +27,22 @@ def ls(limit, host):
 
 @click.command()
 @click.option('--ids', callback=utils.parse_range_callback,
-                      help='ids of crons to be deleted.')
+                      help='IDs of crons to be deleted.')
 def rm(ids):
+    """Delete a crontabs from remote or local system"""
+    click.echo("Delete all crons in the system in range: %s!" % ids)
+
+@click.command()
+@click.option('--ids', callback=utils.parse_range_callback,
+                      help='IDs of crons to be deleted.')
+@click.argument('src_host', nargs=1)
+@click.argument('dst_host', nargs=1)
+def cp(ids, src, dst):
+    """Copy crontabs across servers"""
     click.echo("Delete all crons in the system in range: %s!" % ids)
 
 if __name__ == '__main__':
     crony.add_command(ls)
     crony.add_command(rm)
+    crony.add_command(cp)
     crony()
