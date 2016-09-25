ff88ab5c2f00774aa3e2e88cdd2520e650069487
Jonathan <int3rlop3r@yahoo.in>
2016-09-25 19:49:43 +0530

    Refactored builder.py
    Created a command builder that will only be responsible
    for building a command object, this object can we examined
    before being run.
    
    Separated the command execution part from the command building
    part so that the module could be tested.

--- crony/crontab.py
+++ crony/crontab.py
@@ -1,74 +1,117 @@
 import os
 import subprocess
 
-class Crontab:
+class Command(object):
 
-    def __init__(self, hostname, username=None, port=22, cronuser=None, debug=False):
-        localhostnames = ['localhost', '127.0.0.1', '0.0.0.0']
+    def __init__(self, args, shell=False):
+        self.command_args = args
+        self.shell = shell
 
-        self.port = str(port)
-        self.debug = debug
-        self.cronuser_arg = None
-        self.is_localhost = False
+    @property
+    def command(self):
+        """Display the command as a string"""
+        return " ".join(self.command_args)
+
+    def run(self):
+        """Execute the command and pipes the output to pipe"""
+        return subprocess.Popen(
+                self.command_args, 
+                stdout=subprocess.PIPE, 
+                shell=self.shell)
+
+class CommandBuilder(object):
+    """Builds a Command object using the environment params"""
+
+    def __init__(self, hostname=None, username=None, port=22, cronuser=None):
+
         self.hostname = hostname
-        self.uri = hostname
-
-        if hostname in localhostnames:
-            self.uri = None
-            self.is_localhost = True
+        self.port = str(port)
 
         if username:
-            self.is_localhost = False
             self.username = username
-            self.uri = self.username + '@' + self.hostname
+            self.is_localhost = False
+            self.uri = username + '@' + self.hostname
 
-        if cronuser: # not supported yet!!
-            self.cronuser_arg = "-u" + self.cronuser
+        # not supported yet!!
+        if cronuser:
+            self.cronuser_arg = "-u" + cronuser
+        else:
+            self.cronuser_arg = None
 
-    def _run_command(self, arg=None, command_args=None, shell=False, debug=False):
+    @property
+    def hostname(self):
+        return self._hostname
+
+    @hostname.setter
+    def hostname(self, hostname):
+        if not hostname:
+            hostname = 'localhost'
+
+        if hostname in ['localhost', '127.0.0.1', '0.0.0.0']:
+            self.uri = 'localhost'
+            self.is_localhost = True
+        else:
+            self.uri = hostname
+            self.is_localhost = False
+
+        self._hostname = hostname
+
+    def buildcommand(self, args=None, command_args=None, shell=False):
+        crontab_command = ["crontab"]
+        ssh_command = ["ssh", "-p", self.port, self.uri]
+
         if not command_args:
-            if self.is_localhost and self.cronuser_arg:
-                # interact with another user's crontab
-                command_args = ("crontab", self.cronuser_arg, arg)
-            elif self.is_localhost:
-                command_args = ("crontab", arg)
-            else:
-                command_args = ("ssh", "-p", self.port, self.uri, "crontab", arg)
+            command_args = crontab_command + args
 
-        if debug:
-            print("Debug msg:")
-            print(command_args)
+        if not self.is_localhost:
+            command_args = ssh_command + command_args
 
-        # execute the command and pipe the output to stdout
-        return subprocess.Popen(command_args, stdout=subprocess.PIPE, shell=shell)
+        return Command(args=command_args, shell=shell)
+
+    def list(self):
+        return self.buildcommand(["-l"])
+
+    def remove(self):
+        return self.buildcommand(["-r"])
+
+    def append(self, jobs):
+        shell = True # <- for local
+        command_args = ["(crontab -l 2> /dev/null; "
+                        "printf \"" + jobs + "\") "
+                        "| crontab -"]
+
+        if not self.is_localhost:
+            shell = False  # <- for remote
+
+        return self.buildcommand(command_args=command_args, shell=shell)
+
+    def install(self, jobs):
+        shell = True # <- for local
+        command_args = ["(printf \"" + jobs + "\") | crontab -"]
+        if not self.is_localhost:
+            shell = False  # <- for remote
+
+        return self.buildcommand(command_args=command_args, shell=shell)
+
+class Crontab:
+
+    def __init__(self, **kwargs):
+        self.debug = kwargs.get('debug', False)
+        self.cbuilder = CommandBuilder(**kwargs)
 
     def list(self):
         """Get a list of all the crontabs for the user"""
-        return self._run_command("-l", debug=self.debug)
+        return self.cbuilder.list().run()
 
     def remove(self):
         """Remove all crontabs for the current user"""
-        return self._run_command("-r")
+        return self.cbuilder.remove().run()
 
-    def append(self, s_jobs):
+    def append(self, jobs):
         """Append a crontab"""
-        if self.is_localhost:
-            command_args = "(crontab -l 2> /dev/null; printf \"{}\") | crontab -".format(s_jobs)
-            shell=True
-        else:
-            command_args = ("ssh", "-p", self.port, self.uri, "(crontab -l 2> /dev/null; printf \"{}\") | crontab -".format(s_jobs))
-            shell=False
+        return self.cbuilder.append(jobs).run()
 
-        return self._run_command(command_args=command_args, shell=shell, debug=self.debug)
-
-    def install(self, s_jobs):
+    def install(self, jobs):
         """Install a new crontab overwriting the old one"""
-        if self.is_localhost:
-            command_args = "(printf \"{}\") | crontab -".format(s_jobs)
-            shell=True
-        else:
-            command_args = ("ssh", "-p", self.port, self.uri, "(printf \"{}\") | crontab -".format(s_jobs))
-            shell=False
-
-        return self._run_command(command_args=command_args, shell=shell, debug=self.debug)
+        return self.cbuilder.install(jobs).run()
 
